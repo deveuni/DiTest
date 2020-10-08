5405f5f12c806729f45042acea273bf7d860f800
ITWILL <ITWILL@DESKTOP-KJ97GP7>
2020-10-08 09:00:03 +0900

    스프링

---
+++ DiTest/src/com/itwill/aop/CalcAOPTest.java
@@ -0,0 +1,28 @@
+package com.itwill.aop;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CalcAOPTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" 계산기 실행! ");
+		
+		// AOPTest.xml -> 생성된 객체 정보를 가져다 사용 
+		
+		ApplicationContext appCtx = new ClassPathXmlApplicationContext("AOPTest.xml");
+		
+		Calculator cal2 = (Calculator)appCtx.getBean("calcTarget");
+		cal2.add(200, 200);
+		
+		System.out.println("-----------------------------------------------------");
+		
+		Calculator cal = (Calculator) appCtx.getBean("proxyCalc");
+		cal.add(100, 200);
+		
+		
+
+	}
+
+}
